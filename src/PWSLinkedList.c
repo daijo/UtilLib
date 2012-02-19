3f9bea7cc73577b7ab72dda409f977cb063e6fdb
Daniel Hjort <daniel.hjort@gmail.com>
2012-02-19 18:32:16 +0700

    Expanded the linked list interface and added empty implementations.

--- src/PWSLinkedList.c
+++ src/PWSLinkedList.c
@@ -1,24 +1,86 @@
 #include "PWSLinkedList.h"
 
-typedef struct __Node Node;
-struct __Node {
+typedef struct __ListNode ListNode;
+struct __ListNode {
 	void *data;
-	Node *previous;
-	Node *next;
+	ListNode *previous;
+	ListNode *next;
 };
 
 struct __PWSLinkedList {
-	Node *root;
+	ListNode *head;
+        ListNode *tail;
+        int count;
 };
 
-void addLast(void* data, PWSLinkedList* list)
+PWSLinkedList* initLinkedList()
 {
-
 }
 
-void addFirst(void* data, PWSLinkedList* list)
+void freeLinkedList(PWSLinkedList *list) /* free remaining data? */
 {
-
 }
 
+int addLast(PWSLinkedList *list, PWSData* data)
+{
+}
 
+void addFirst(PWSLinkedList *list, PWSData* data)
+{
+}
+
+int addAtIndex(PWSLinkedList *list, PWSData* data, int index)
+{
+}
+
+PWSData* getFirst(PWSLinkedList* list)
+{
+}
+
+PWSLinkedList* getRest(PWSLinkedList* list)
+{
+}
+
+PWSData* getLast(PWSLinkedList* list)
+{
+}
+
+PWSData* getByReference(PWSLinkedList* list, PWSData* data)
+{
+}
+
+PWSData* getByValue(PWSLinkedList* list, PWSData* data)
+{
+}
+
+PWSData* getByIndex(PWSLinkedList* list, int index)
+{
+}
+
+PWSData* removeFirst(PWSLinkedList* list)
+{
+}
+
+PWSData* removeLast(PWSLinkedList* list)
+{
+}
+
+PWSData* removeByReference(PWSLinkedList* list, PWSData* data)
+{
+}
+
+PWSData* removeByValue(PWSLinkedList* list, PWSData* data)
+{
+}
+
+PWSData* removeByIndex(PWSLinkedList* list, int index)
+{
+}
+
+int count(PWSLinkedList *list)
+{
+}
+
+bool isEmpty(PWSLinkedList *list)
+{
+}
--- src/include/PWSLinkedList.h
+++ src/include/PWSLinkedList.h
@@ -1,9 +1,28 @@
+#include <stdbool.h>
+
+#include "PWSData.h"
+
 typedef struct __PWSLinkedList PWSLinkedList;
 
 PWSLinkedList* initLinkedList();
-void releaseLinkedList(PWSLinkedList list);
-void addLast(void* data, PWSLinkedList* list);
-void addFirst(void* data, PWSLinkedList* list);
-void* getFirst(PWSLinkedList* list);
-void* getLast(PWSLinkedList* list);
-void* remove(PWSLinkedList* list, void* data); 
+void freeLinkedList(PWSLinkedList *list); /* free remaining data? */
+
+int addLast(PWSLinkedList *list, PWSData* data);
+void addFirst(PWSLinkedList *list, PWSData* data);
+int addAtIndex(PWSLinkedList *list, PWSData* data, int index);
+
+PWSData* getFirst(PWSLinkedList* list);
+PWSLinkedList* getRest(PWSLinkedList* list);
+PWSData* getLast(PWSLinkedList* list);
+PWSData* getByReference(PWSLinkedList* list, PWSData* data);
+PWSData* getByValue(PWSLinkedList* list, PWSData* data);
+PWSData* getByIndex(PWSLinkedList* list, int index);
+
+PWSData* removeFirst(PWSLinkedList* list);
+PWSData* removeLast(PWSLinkedList* list);
+PWSData* removeByReference(PWSLinkedList* list, PWSData* data);
+PWSData* removeByValue(PWSLinkedList* list, PWSData* data);
+PWSData* removeByIndex(PWSLinkedList* list, int index);
+
+int count(PWSLinkedList *list);
+bool isEmpty(PWSLinkedList *list);
